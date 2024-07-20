6801ce1f613bd6987dcfc916f1378d330329d10c
Yan Melo <yandemelo@yahoo.com.br>
2024-07-20 02:57:17 -0300

    feat: Limitando o tamanho do cabo (caso seja informado tamanho máximo) e retornando as possíveis conexões

--- src/Conexao.java
+++ src/Conexao.java
@@ -3,14 +3,16 @@ public class Conexao {
     private String nome;
     private int xConexao;
     private int yConexao;
+    private String tipo;
 
     public Conexao() {
     }
 
-    public Conexao(String nome, int xConexao, int yConexao) {
+    public Conexao(String nome, int xConexao, int yConexao, String tipo) {
         this.nome = nome;
         this.xConexao = xConexao;
         this.yConexao = yConexao;
+        this.tipo = tipo;
     }
 
     public String getNome() {
@@ -37,4 +39,12 @@ public class Conexao {
         this.yConexao = yConexao;
     }
 
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
 }
--- src/CustomVisitor.java
+++ src/CustomVisitor.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomVisitor extends ExprBaseVisitor<Object> {
-    
+
     private GraphGUI grafo;
     private String distanciaCabo;
 
     private List<Conexao> conexoesList = new ArrayList<Conexao>();
 
     public CustomVisitor() {
-        
+
     }
 
     // GRAFOS
     @Override
-    public Object visitGrafo(ExprParser.GrafoContext ctx){
+    public Object visitGrafo(ExprParser.GrafoContext ctx) {
         this.grafo = new GraphGUI(800, 800);
         return visitChildren(ctx);
     }
@@ -43,7 +43,7 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int width = Integer.parseInt(ctx.areaX.getText());
         int height = Integer.parseInt(ctx.areaY.getText());
         if (ctx.distanciaCabo == null) {
-            distanciaCabo = "20";
+            distanciaCabo = "100";
         } else {
             distanciaCabo = ctx.distanciaCabo.getText();
         }
@@ -58,7 +58,7 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int x = Integer.parseInt(ctx.x.getText());
         int y = Integer.parseInt(ctx.y.getText());
 
-        conexoesList.add(new Conexao(nome, x, y));
+        conexoesList.add(new Conexao(nome, x, y, "modem"));
 
         grafo.addVertex(nome, x, y);
         return visitChildren(ctx);
@@ -71,18 +71,19 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int ySwitch = Integer.parseInt(ctx.y.getText());
         String conectado = ctx.conectado.getText();
         String distancia = distanciaCabo;
-        
+
         for (Conexao modem : conexoesList) {
             if (modem.getNome().equals(conectado)) {
                 Conexao modemConectado = modem;
-                distancia = calcularDistancia(xSwitch, modemConectado.getXConexao(), ySwitch, modemConectado.getYConexao()).toString();
+                distancia = calcularDistancia(xSwitch, modemConectado.getXConexao(), ySwitch,
+                        modemConectado.getYConexao()).toString();
                 break;
             }
         }
-        
+
         grafo.addVertex(nome, xSwitch, ySwitch);
-        grafo.addEdge(distancia, nome, conectado);
-        conexoesList.add(new Conexao(nome, xSwitch, ySwitch));
+        grafo.addEdge("Cabo - "+ distancia, nome, conectado);
+        conexoesList.add(new Conexao(nome, xSwitch, ySwitch, "switch"));
 
         return visitChildren(ctx);
     }
@@ -94,39 +95,65 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int yRep = Integer.parseInt(ctx.y.getText());
         String conectado = ctx.conectado.getText();
         String distancia = distanciaCabo;
-    
+
         for (Conexao modem : conexoesList) {
             if (modem.getNome().equals(conectado)) {
                 Conexao modemConectado = modem;
-                distancia = calcularDistancia(xRep, modemConectado.getXConexao(), yRep, modemConectado.getYConexao()).toString();
+                distancia = calcularDistancia(xRep, modemConectado.getXConexao(), yRep, modemConectado.getYConexao())
+                        .toString();
                 break;
             }
         }
 
         grafo.addVertex(nome, xRep, yRep);
-        conexoesList.add(new Conexao(nome, xRep, yRep));
-        grafo.addEdge(distancia, nome, conectado);
+        conexoesList.add(new Conexao(nome, xRep, yRep, "repetidor"));
+        grafo.addEdge("Wi-fi - "+ distancia, nome, conectado);
         return visitChildren(ctx);
     }
 
     @Override
-    public Object visitDispositivo(ExprParser.DispositivoContext ctx){
+    public Object visitDispositivo(ExprParser.DispositivoContext ctx) {
         String nome = ctx.nome.getText();
         int xDisp = Integer.parseInt(ctx.x.getText());
         int yDisp = Integer.parseInt(ctx.y.getText());
-        Long distanciaModem = Long.MAX_VALUE;
-        String modemNome = "";
-        grafo.addVertex(nome, xDisp, yDisp);
+        Long distanciaConexao = Long.MAX_VALUE;
+        String conexaoNome = "";
+        String nomeAresta = "";
 
-        for (Conexao modem : conexoesList) {
-            Long distanciaCalculada = calcularDistancia(xDisp, modem.getXConexao(), yDisp, modem.getYConexao());
-            if (distanciaCalculada < distanciaModem) {
-                distanciaModem = distanciaCalculada;
-                modemNome = modem.getNome();
+        for (Conexao conexao : conexoesList) {
+            Long distanciaCalculada = calcularDistancia(xDisp, conexao.getXConexao(), yDisp, conexao.getYConexao());
+            if (distanciaCalculada < distanciaConexao) {
+                switch (conexao.getTipo()) {
+                    case "modem":
+                        distanciaConexao = distanciaCalculada;
+                        conexaoNome = conexao.getNome();
+                        if (distanciaCalculada <= Long.parseLong(distanciaCabo)) {
+                            nomeAresta = "Cabo ou Wi-fi - "+ distanciaConexao.toString();
+                        } else {
+                            nomeAresta = "Wi-fi - "+ distanciaConexao.toString();
+                        }
+                        break;
+                    case "repetidor":
+                        distanciaConexao = distanciaCalculada;
+                        conexaoNome = conexao.getNome();
+                        nomeAresta = "Wi-fi - "+ distanciaConexao.toString();
+                        break;
+                        case "switch":
+                        if (distanciaCalculada <= Long.parseLong(distanciaCabo)) {
+                            distanciaConexao = distanciaCalculada;
+                            conexaoNome = conexao.getNome();
+                            nomeAresta = "Cabo - "+ distanciaConexao.toString();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
             }
         }
-        
-        grafo.addEdge(distanciaModem.toString(), nome, modemNome);
+
+        grafo.addVertex(nome, xDisp, yDisp);
+        grafo.addEdge(nomeAresta, nome, conexaoNome);
         return visitChildren(ctx);
     }
 
