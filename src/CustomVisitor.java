e8cde6f3712ac4c23e9d4885464c15a4d72ebb0f
Yan Melo <yandemelo@yahoo.com.br>
2024-07-27 09:34:28 -0300

    Criando arquivos .txt para serem utilizados como exemplo de funcionamento do projeto

--- src/CustomVisitor.java
+++ src/CustomVisitor.java
@@ -22,7 +22,10 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     // GRAFOS
     @Override
     public Object visitGrafo(ExprParser.GrafoContext ctx) {
-        this.grafo = new GraphGUI(800, 800);
+        Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+        int alturaTela = (int) tela.getHeight();
+        int larguraTela = (int) tela.getWidth();
+        this.grafo = new GraphGUI(alturaTela, larguraTela);
         return visitChildren(ctx);
     }
 
@@ -60,15 +63,15 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int alturaTela = (int) tela.getHeight();
         int larguraTela = (int) tela.getWidth();
 
-        if (height > alturaTela && height >= 2500) {
+        if (height > alturaTela && height >= 1000) {
             escalaY = height / alturaTela;
         } else {
-            escalaY = 2.3;
+            escalaY = alturaTela / height;
         }
-        if (width > larguraTela && height >= 2500) {
+        if (width > larguraTela && height >= 1000) {
             escalaX = width / larguraTela;
         } else {
-            escalaX = 1.3;
+            escalaX = larguraTela / width;
         }
 
         this.grafo = new GraphGUI(alturaTela, larguraTela);
@@ -78,10 +81,13 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     @Override
     public Object visitModem(ExprParser.ModemContext ctx) {
         String nome = ctx.nome.getText();
-        Double x = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double y = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double x = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double y = Integer.parseInt(ctx.y.getText()) * escalaY;
 
-        conexoesList.add(new Conexao(nome, x.intValue(), y.intValue(), "modem"));
+        //Para os valores das distancias
+        Double xAtual = x/escalaX;
+        Double yAtual = y/escalaX;
+        conexoesList.add(new Conexao(nome, xAtual.intValue(), yAtual.intValue(), "modem"));
 
         grafo.addVertex(nome, x.intValue(), y.intValue(), "conexao");
         return visitChildren(ctx);
@@ -90,15 +96,19 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     @Override
     public Object visitSwitch(ExprParser.SwitchContext ctx) {
         String nome = ctx.nome.getText();
-        Double xSwitch = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double ySwitch = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double xSwitch = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double ySwitch = Integer.parseInt(ctx.y.getText()) * escalaY;
         String conectado = ctx.conectado.getText();
         String distancia = distanciaCabo;
+
+        //Para os valores das distancias
+        Double xAtual = xSwitch/escalaX;
+        Double yAtual = ySwitch/escalaY;
 
         for (Conexao modem : conexoesList) {
             if (modem.getNome().equals(conectado)) {
                 Conexao modemConectado = modem;
-                distancia = calcularDistancia(xSwitch.intValue(), modemConectado.getXConexao(), ySwitch.intValue(),
+                distancia = calcularDistancia(xAtual.intValue(), modemConectado.getXConexao(), yAtual.intValue(),
                         modemConectado.getYConexao()).toString();
                 break;
             }
@@ -106,7 +116,7 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
 
         grafo.addVertex(nome, xSwitch.intValue(), ySwitch.intValue(), "conexao");
         grafo.addEdge("Cabo - " + distancia, nome, conectado);
-        conexoesList.add(new Conexao(nome, xSwitch.intValue(), ySwitch.intValue(), "switch"));
+        conexoesList.add(new Conexao(nome, xAtual.intValue(), yAtual.intValue(), "switch"));
 
         return visitChildren(ctx);
     }
@@ -114,22 +124,27 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     @Override
     public Object visitRepetidor(ExprParser.RepetidorContext ctx) {
         String nome = ctx.nome.getText();
-        Double xRep = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double yRep = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double xRep = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double yRep = Integer.parseInt(ctx.y.getText()) * escalaY;
+
+        //Para os valores das distancias
+        Double xAtual = xRep/escalaX;
+        Double yAtual = yRep/escalaY;
+
         String conectado = ctx.conectado.getText();
         String distancia = distanciaCabo;
 
         for (Conexao modem : conexoesList) {
             if (modem.getNome().equals(conectado)) {
                 Conexao modemConectado = modem;
-                distancia = calcularDistancia(xRep.intValue(), modemConectado.getXConexao(), yRep.intValue(), modemConectado.getYConexao())
+                distancia = calcularDistancia(xAtual.intValue(), modemConectado.getXConexao(), yAtual.intValue(), modemConectado.getYConexao())
                         .toString();
                 break;
             }
         }
 
         grafo.addVertex(nome, xRep.intValue(), yRep.intValue(), "conexao");
-        conexoesList.add(new Conexao(nome, xRep.intValue(), yRep.intValue(), "repetidor"));
+        conexoesList.add(new Conexao(nome, xAtual.intValue(), yAtual.intValue(), "repetidor"));
         grafo.addEdge("Wi-fi - " + distancia, nome, conectado);
         return visitChildren(ctx);
     }
@@ -137,14 +152,19 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     @Override
     public Object visitDispositivo(ExprParser.DispositivoContext ctx) {
         String nome = ctx.nome.getText();
-        Double xDisp = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double yDisp = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double xDisp = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double yDisp = Integer.parseInt(ctx.y.getText()) * escalaY;
+
+        //Para os valores das distancias
+        Double xAtual = xDisp/escalaX;
+        Double yAtual = yDisp/escalaY;
+
         Long distanciaConexao = Long.MAX_VALUE;
         String conexaoNome = "";
         String nomeAresta = "";
 
         for (Conexao conexao : conexoesList) {
-            Long distanciaCalculada = calcularDistancia(xDisp.intValue(), conexao.getXConexao(), yDisp.intValue(), conexao.getYConexao());
+            Long distanciaCalculada = calcularDistancia(xAtual.intValue(), conexao.getXConexao(), yAtual.intValue(), conexao.getYConexao());
             if (distanciaCalculada < distanciaConexao) {
                 switch (conexao.getTipo()) {
                     case "modem":
@@ -192,14 +212,14 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
         int larguraTela = (int) tela.getWidth();
 
         if (height > alturaTela && height >= 2000) {
-            escalaY = (height / alturaTela)+0.2;
+            escalaY = (height / alturaTela)/3;
         } else {
-            escalaY = 2.3;
+            escalaY = (alturaTela / height)-0.2;
         }
         if (width > larguraTela && height >= 2000) {
-            escalaX = (width / larguraTela)+0.1;
+            escalaX = (width / larguraTela)/2;
         } else {
-            escalaX = 1.3;
+            escalaX = (larguraTela / width)-0.2;
         }
 
         this.grafo = new GraphGUI(alturaTela, larguraTela);
@@ -210,10 +230,14 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     public Object visitCasa(ExprParser.CasaContext ctx) {
         String numero = ctx.numeroCasa.getText();
 
-        Double xCasa = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double yCasa = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double xCasa = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double yCasa = Integer.parseInt(ctx.y.getText()) * escalaY;
 
-        casasList.add(new Casa(numero, xCasa.intValue(), yCasa.intValue()));
+        //Para os valores das distancias
+        Double xAtual = xCasa/escalaX;
+        Double yAtual = yCasa/escalaY;
+
+        casasList.add(new Casa(numero, xAtual.intValue(), yAtual.intValue()));
 
         grafo.addVertex(numero, xCasa.intValue(), yCasa.intValue(), "");
         return visitChildren(ctx);
@@ -222,15 +246,19 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     @Override
     public Object visitTorre(ExprParser.TorreContext ctx) {
         String nomeTorre = ctx.nomeTorre.getText();
-        Double xTorre = Integer.parseInt(ctx.x.getText()) / escalaX;
-        Double yTorre = Integer.parseInt(ctx.y.getText()) / escalaY;
+        Double xTorre = Integer.parseInt(ctx.x.getText()) * escalaX;
+        Double yTorre = Integer.parseInt(ctx.y.getText()) * escalaY;
         Long distanciaDaCasa;
         String numeroCasa = "";
         alcanceTorre = Integer.parseInt(ctx.alcance.getText());
 
+        //Para os valores das distancias
+        Double xAtual = xTorre/escalaX;
+        Double yAtual = yTorre/escalaY;
+
         grafo.addVertex(nomeTorre, xTorre.intValue(), yTorre.intValue(), "torre");
         for(Casa casa : casasList){
-            distanciaDaCasa = calcularDistancia(xTorre.intValue(), casa.getXCasa(), yTorre.intValue(), casa.getYCasa());
+            distanciaDaCasa = calcularDistancia(xAtual.intValue(), casa.getXCasa(), yAtual.intValue(), casa.getYCasa());
             numeroCasa = casa.getNumero();
 
             if (distanciaDaCasa <= alcanceTorre*0.9) {
@@ -242,8 +270,8 @@ public class CustomVisitor extends ExprBaseVisitor<Object> {
     }
 
     public Long calcularDistancia(int x1, int x2, int y1, int y2) {
-        double deltaX = (x1 - x2) * escalaX;
-        double deltaY = (y1 - y2) * escalaY;
+        double deltaX = (x1 - x2);
+        double deltaY = (y1 - y2);
         double distancia = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
         return Math.round(distancia);
     }
